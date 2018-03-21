3c24dc184e216a51fa49a82bb40a46af0d018e34
Jens Streck <jens.streck@gmail.com>
2018-03-21 13:33:40 -0500

    burger own git

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,114 @@
+import React, {Component} from 'react';
+import { connect } from "react-redux";
+import Aux from '../../hoc/Aux'
+import axios from '../../axios-orders';
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import Spinner from '../../components/UI/Spinner/Spinner'
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+import * as actions from '../../store/actions/index';
+
+class BurgerBuilder extends Component {
+
+    state = {
+        purchasing: false
+    }
+
+    componentDidMount() {
+        this.props.onInitIngredients();
+    }
+    updatePurchaseState = (ingredients) => {
+        const sum = Object.keys(ingredients).map(igKey => {
+            return ingredients[igKey]
+        }).reduce((sum, el) => {
+            return sum + el;
+        },0);
+        return sum > 0;
+    }
+
+    purchaseHandler = () => {
+        if(this.props.isAuthenticated) {
+            this.setState({purchasing: true});
+        } else {
+            this.props.onSetAuthRedirectPath('/checkout');
+            this.props.history.push('/auth');
+        }
+    }
+
+    purchaseCancelHandler = () => {
+        this.setState({purchasing: false});
+    }
+
+    purchaseContinueHandler = () => {
+        // alert('You Continue!');
+
+        this.props.onInitPurchase();
+        this.props.history.push('/checkout');
+    }
+
+    render() {
+        const disabledInfo = {
+            ...this.props.ing
+        };
+        for(let key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <= 0
+        }
+        let orderSummary = null;
+
+        let burger = this.props.error ? <p> Ingredinets can't be loaded</p> : <Spinner />
+        if (this.props.ings) {
+            burger = (
+                <Aux>
+                    <Burger ingredients={this.props.ings}/>
+                    <BuildControls 
+                        isAuth={this.props.isAuthenticated}
+                        price={this.props.price}
+                        purchaseable={this.updatePurchaseState(this.props.ings)}
+                        ingredientAdded={this.props.onIngredientAdded} 
+                        ingredientRemoved={this.props.onIngredientRemove}
+                        ordered={this.purchaseHandler}
+                        disabled={disabledInfo} />
+                </Aux>
+            );
+
+            orderSummary = <OrderSummary 
+                ingredients={this.props.ings}
+                purchaseCancelled={this.purchaseCancelHandler}
+                purchaseContinued={this.purchaseContinueHandler}
+                price={this.props.price.toFixed(2)} />
+        }
+
+
+        return (
+            <Aux>
+                <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
+                   {orderSummary}
+                </Modal>
+                {burger}
+            </Aux>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        ings: state.burgerBuilder.ingredients,
+        price: state.burgerBuilder.totalPrice,
+        error: state.burgerBuilder.error,
+        isAuthenticated: state.auth.token !== null
+    };
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onIngredientAdded: (ingName) => dispatch(actions.addIngredient(ingName)),
+        onIngredientRemove: (ingName) => dispatch(actions.removeIngredient(ingName)),
+        onInitIngredients: () => dispatch(actions.initIngredients()),
+        onInitPurchase: () => dispatch(actions.purchaseInit()),
+        onSetAuthRedirectPath: (path) => dispatch(actions.setAuthRedirectPath(path))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(BurgerBuilder, axios));
